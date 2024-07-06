ebc91b39795f595ff1533c82c21d120de419a918
Tristan <73254843+THG20203@users.noreply.github.com>
2024-07-06 14:57:46 +0100

    concatenate

--- index.php
+++ index.php
@@ -8,9 +8,8 @@
 <body>
   <h1>
     <?php
-    // whateverr occurs between these blocks should be interpreted as php rather than html
-    // Hello World wouldn't work, so have ot print a string with echo and "" 
-    echo "Hello World";
+    // concatenate
+    echo "Hello, " . "Tristan";
     ?>
   </h1>
 </body>
