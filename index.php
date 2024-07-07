e4a8cb4f08f65df9002e944bece6189a3fe60b3b
Tristan <73254843+THG20203@users.noreply.github.com>
2024-07-07 13:39:06 +0100

    more conditional work

--- index.php
+++ index.php
@@ -3,13 +3,23 @@
 
 <head>
   <title>PHP learning</title>
+  <style>
+    body {
+      display: grid;
+      place-items: center;
+      height: 100vh;
+      margin: 0;
+      font-family: sans-serif;
+    }
+  </style>
 </head>
 
 <body>
+  <?php
+  $name = "A book";
+  ?>
   <h1>
-    <?php
-
-    ?>
+    You have read <?php echo $name; ?>
   </h1>
 </body>
 
