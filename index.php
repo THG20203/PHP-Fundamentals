65eb8db4ecc09e4dadffa128ecabbbf8a8780242
Tristan <73254843+THG20203@users.noreply.github.com>
2024-07-09 09:19:36 +0100

    php learning associative arrays further

--- index.php
+++ index.php
@@ -16,9 +16,15 @@
       "purchaseUrl" => "https://example.com"
     ],
     [
-      "name" => "The Langoliers",
-      "author" => "Hail Mary",
+      "name" => "Hail Mary",
+      "author" => "Andy Weir",
       "releaseYear" => 2021,
+      "purchaseUrl" => "https://example.com"
+    ],
+    [
+      "name" => "The Martian",
+      "author" => "Andy Weir",
+      "releaseYear" => 2011,
       "purchaseUrl" => "https://example.com"
     ],
   ];
@@ -27,7 +33,7 @@
   <?php foreach ($books as $book) : ?>
     <li>
       <a href="<?= $book['purchaseUrl'] ?>">
-        <?= $book["name"]; ?>(<?= $book["releaseYear"] ?>)
+        <?= $book["name"]; ?> (<?= $book["releaseYear"] ?>)
       </a>
     </li>
   <?php endforeach; ?>
