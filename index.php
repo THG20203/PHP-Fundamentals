e11f9aec71b289f8342b571be631f5dee99e2eba
Tristan <73254843+THG20203@users.noreply.github.com>
2024-07-09 10:14:15 +0100

    removed if statement

--- index.php
+++ index.php
@@ -41,13 +41,11 @@
 
   <ul>
     <?php foreach ($books as $book) : ?>
-      <?php if ($book['author'] === "Andy Weir") : ?>
-        <li>
-          <a href="<?= $book['purchaseUrl'] ?>">
-            <?= $book["name"]; ?> (<?= $book["releaseYear"] ?>) - By <?= $book["author"] ?>
-          </a>
-        </li>
-      <?php endif; ?>
+      <li>
+        <a href="<?= $book['purchaseUrl'] ?>">
+          <?= $book["name"]; ?> (<?= $book["releaseYear"] ?>) - By <?= $book["author"] ?>
+        </a>
+      </li>
     <?php endforeach; ?>
   </ul>
 
