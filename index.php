5832635dedca68527b9870a0d3584fd82340935f
Tristan <73254843+THG20203@users.noreply.github.com>
2024-07-11 17:02:26 +0100

    removing comments that weren't needed

--- index.php
+++ index.php
@@ -32,18 +32,11 @@ php -S localhost:8888      -->
     ],
   ];
 
-  // Create function - (functions like the verbs of the programming language, they are to be called,
-  // they then do something, then they return results)  
   function filterByAuthor($books, $author)
   {
     $filteredBooks = [];
-
-    // A foreach loop is used to iterate over each book in the $books array. For each iteration, the 
-    // current book is assigned to the variable $book.
     foreach ($books as $book) {
       if ($book["author"] === $author) {
-        // If the condition in the if statement is true (i.e., the author is "Andy Weir"), the current book is 
-        // added to the $filteredBooks array using the [] syntax, which appends the book to the array.
         $filteredBooks[] = $book;
       }
     }
@@ -66,5 +59,3 @@ php -S localhost:8888      -->
 </body>
 
 </html>
-
-<!-- remember = single equals is assigning. === is for checking for equality -->
